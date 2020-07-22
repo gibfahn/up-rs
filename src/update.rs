4470854eada420efb80ff4987db88dee38e7157b
Gibson Fahnestock <gibfahn@gmail.com>
2020-07-22 10:55:22 +0100

    feat(update): allow inheriting env and referring to existing env vars
    Allows you to pull in env vars from your environment, and also to refer
    to vars in other vars.

--- src/update.rs
+++ src/update.rs
@@ -17,10 +17,7 @@ use displaydoc::Display;
 use log::{debug, error, info, trace};
 use thiserror::Error;
 
-use crate::{
-    config::{self, ConfigToml},
-    tasks::TaskError,
-};
+use crate::{config, tasks::TaskError};
 
 mod task;
 
@@ -37,7 +34,10 @@ pub fn update(config: &config::UpConfig, filter_tasks: &Option<Vec<String>>) -> 
     tasks_dir.pop();
     tasks_dir.push("tasks");
 
-    let env = get_env(&config.config_toml)?;
+    let env = get_env(
+        config.config_toml.inherit_env.as_ref(),
+        config.config_toml.env.as_ref(),
+    )?;
 
     if config.config_toml.needs_sudo {
         // TODO(gib): this only lasts for 5 minutes.
@@ -85,28 +85,33 @@ pub fn update(config: &config::UpConfig, filter_tasks: &Option<Vec<String>>) -> 
     run_tasks(tasks, &env)
 }
 
-fn get_env(config_toml: &ConfigToml) -> Result<HashMap<String, String>> {
+// TODO(gib): add tests for cyclical config values etc.
+fn get_env(
+    inherit_env: Option<&Vec<String>>,
+    input_env: Option<&HashMap<String, String>>,
+) -> Result<HashMap<String, String>> {
     let mut env: HashMap<String, String> = HashMap::new();
-    if let Some(inherited_env) = config_toml.inherit_env.as_ref() {
+    if let Some(inherited_env) = inherit_env {
         for inherited_var in inherited_env {
-            if let Ok(value) = std::env::var(&inherited_var) {
-                env.insert(inherited_var.clone(), value);
+            if let Ok(value) = std::env::var(inherited_var) {
+                env.insert(inherited_var.to_owned(), value);
             }
         }
     }
 
     let mut unresolved_env = Vec::new();
 
-    if let Some(config_env) = config_toml.env.as_ref() {
-        trace!("Unresolved env: {:?}", config_env);
+    if let Some(config_env) = input_env {
+        trace!("Provided env: {:#?}", config_env);
+        let mut calculated_env = HashMap::new();
         for (key, val) in config_env.iter() {
-            env.insert(
+            calculated_env.insert(
                 key.clone(),
                 shellexpand::full_with_context(val, dirs::home_dir, |k| match env.get(k) {
                     Some(val) => Ok(Some(val)),
                     None => {
                         if config_env.contains_key(k) {
-                            unresolved_env.push(k.to_owned());
+                            unresolved_env.push(key.to_owned());
                             Ok(None)
                         } else {
                             Err(anyhow!(
@@ -123,8 +128,62 @@ fn get_env(config_toml: &ConfigToml) -> Result<HashMap<String, String>> {
                 .into_owned(),
             );
         }
+        calculated_env.drain().for_each(|(k, v)| {
+            env.insert(k, v);
+        });
     }
-    debug!("Expanded config env: {:?}", env);
+
+    // Resolve unresolved env vars.
+    debug!("Unresolved env: {:?}", unresolved_env);
+    while !unresolved_env.is_empty() {
+        trace!("Env so far: {:#?}", env);
+        trace!("Still unresolved env: {:#?}", unresolved_env);
+        let mut resolved_indices = Vec::new();
+        for (index, key) in unresolved_env.iter().enumerate() {
+            let val = env
+                .get(key)
+                .ok_or_else(|| anyhow!("How did we get here?"))?;
+            let resolved_val = shellexpand::env_with_context(val, |k| {
+                if unresolved_env.iter().any(|s| s == k) {
+                    Ok(None)
+                } else if let Some(v) = env.get(k) {
+                    resolved_indices.push(index);
+                    Ok(Some(v))
+                } else {
+                    Err(anyhow!("Shouldn't be possible to hit this."))
+                }
+            })
+            .map_err(|e| UpdateError::EnvLookup {
+                var: e.var_name,
+                source: e.cause,
+            })?
+            .into_owned();
+            let val_ref = env
+                .get_mut(key)
+                .ok_or_else(|| anyhow!("How did we get here?"))?;
+            *val_ref = resolved_val;
+        }
+        trace!("resolved indices: {:?}", resolved_indices);
+        if resolved_indices.is_empty() {
+            bail!(
+                "Errors resolving env, do you have cycles? Unresolved env: {:#?}",
+                unresolved_env
+            );
+        }
+        unresolved_env = unresolved_env
+            .into_iter()
+            .enumerate()
+            .filter_map(|(index, value)| {
+                if resolved_indices.contains(&index) {
+                    None
+                } else {
+                    Some(value)
+                }
+            })
+            .collect();
+    }
+
+    debug!("Expanded config env: {:#?}", env);
     Ok(env)
 }
 
