25dfe4b9a7c16ec15ba587e51d2e410b09d6bdf8
Gibson Fahnestock <gibfahn@gmail.com>
2023-02-06 08:50:01 +0000

    test(help): update to new clap-generated help content

--- tests/help.rs
+++ tests/help.rs
@@ -6,19 +6,27 @@ use predicates::prelude::*;
 fn help_test() {
     let temp_dir = testutils::temp_dir("up", testutils::function_path!()).unwrap();
 
-    check_help_or_version("-h", &temp_dir);
-    check_help_or_version("--help", &temp_dir);
+    check_help("-h", &temp_dir);
+    check_help("--help", &temp_dir);
 }
 
 #[test]
 fn version_test() {
     let temp_dir = testutils::temp_dir("up", testutils::function_path!()).unwrap();
 
-    check_help_or_version("-V", &temp_dir);
-    check_help_or_version("--version", &temp_dir);
+    check_version("-V", &temp_dir);
+    check_version("--version", &temp_dir);
 }
 
-fn check_help_or_version(arg: &str, temp_dir: &Path) {
+fn check_help(arg: &str, temp_dir: &Path) {
+    let mut cmd = testutils::test_binary_cmd("up", temp_dir);
+    cmd.arg(arg);
+    cmd.assert().success().stdout(predicate::str::starts_with(
+        "Up is a tool to help you manage your developer machine.",
+    ));
+}
+
+fn check_version(arg: &str, temp_dir: &Path) {
     let mut cmd = testutils::test_binary_cmd("up", temp_dir);
     cmd.arg(arg);
     cmd.assert()
