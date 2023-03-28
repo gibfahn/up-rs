535510d5c7525410078621f25dff4345d20bcf3d
Gibson Fahnestock <gibfahn@gmail.com>
2023-03-28 15:06:55 +0100

    test(defaults): add a test for array-of-dicts with `...`

--- tests/defaults.rs
+++ tests/defaults.rs
@@ -113,13 +113,13 @@ struct TestCase {
     defaults_type: &'static str,
     /// Original value in defaults format (as used by `defaults write`). Space-separate dicts and
     /// arrays.
-    orig_value: &'static str,
+    orig_defaults_set_value: &'static str,
     /// Original value in yaml format (as returned by `up defaults read`).
-    orig_check_value: &'static str,
+    orig_up_check_value: &'static str,
     /// New value in yaml format (as used by `up defaults write`).
-    new_value: &'static str,
+    up_set_value: &'static str,
     /// New value in defaults format (as used by `defaults read`).
-    check_value: &'static str,
+    defaults_check_value: &'static str,
 }
 
 #[test]
@@ -132,80 +132,158 @@ fn test_defaults_write_local() {
         TestCase {
             name: "basic_bool",
             defaults_type: "bool",
-            orig_value: "true",
-            orig_check_value: "true",
-            new_value: "false",
-            check_value: "0",
+            orig_defaults_set_value: "true",
+            orig_up_check_value: "true",
+            up_set_value: "false",
+            defaults_check_value: "0",
         },
         TestCase {
             name: "basic_int",
             defaults_type: "int",
-            orig_value: "5",
-            orig_check_value: "5",
-            new_value: "10",
-            check_value: "10",
+            orig_defaults_set_value: "5",
+            orig_up_check_value: "5",
+            up_set_value: "10",
+            defaults_check_value: "10",
         },
         TestCase {
             name: "basic_float",
             defaults_type: "float",
-            orig_value: "5.123",
-            orig_check_value: "5.123000144958496",
-            new_value: "7.8",
-            check_value: "7.8",
+            orig_defaults_set_value: "5.123",
+            orig_up_check_value: "5.123000144958496",
+            up_set_value: "7.8",
+            defaults_check_value: "7.8",
         },
         TestCase {
             name: "basic_string",
             defaults_type: "string",
-            orig_value: "initial value\nline 2",
-            orig_check_value: "|-\n  initial value\n  line 2",
-            new_value: r#""new value\nnew line 2""#,
-            check_value: "new value\nnew line 2",
+            orig_defaults_set_value: "initial basic_string value\nline 2",
+            orig_up_check_value: "|-\n  initial basic_string value\n  line 2",
+            up_set_value: r#""new value\nnew line 2""#,
+            defaults_check_value: "new value\nnew line 2",
         },
         // Check swapping an array to a dict works.
         TestCase {
             name: "array_to_dict",
             defaults_type: "array",
-            orig_value: "a b c",
-            orig_check_value: "- a\n- b\n- c",
-            new_value: "x: four\ny: five\nz: six",
-            check_value: "{\n    x = four;\n    y = five;\n    z = six;\n}",
+            orig_defaults_set_value: "(aarray_to_dict, barray_to_dict, carray_to_dict)",
+            orig_up_check_value: "- aarray_to_dict\n- barray_to_dict\n- carray_to_dict",
+            up_set_value: "x: fourarray_to_dict\ny: fivearray_to_dict\nz: sixarray_to_dict",
+            defaults_check_value: r#"{
+    x = "fourarray_to_dict";
+    y = "fivearray_to_dict";
+    z = "sixarray_to_dict";
+}"#,
         },
         // Check swapping an array to a dict works.
         TestCase {
             name: "array_to_dict_2",
             defaults_type: "array",
-            orig_value: "a b c",
-            orig_check_value: "- a\n- b\n- c",
-            new_value: "x: four\ny: five\nz: six",
-            check_value: "{\n    x = four;\n    y = five;\n    z = six;\n}",
+            orig_defaults_set_value: "(d, e, f)",
+            orig_up_check_value: "- d\n- e\n- f",
+            up_set_value: "x: four\ny: five\nz: six",
+            defaults_check_value: "{\n    x = four;\n    y = five;\n    z = six;\n}",
         },
         // Check swapping a dict to an array works.
         TestCase {
             name: "dict_to_array",
             defaults_type: "dict",
-            orig_value: "u one v two w three",
-            orig_check_value: "u: one\nv: two\nw: three",
-            new_value: "['d','e','f']",
-            check_value: "(\n    d,\n    e,\n    f\n)",
+            orig_defaults_set_value: "{ u = one; v = two; w = three; }",
+            orig_up_check_value: "u: one\nv: two\nw: three",
+            up_set_value: "['d','e','f']",
+            defaults_check_value: "(\n    d,\n    e,\n    f\n)",
         },
         // Check preserving original array values works.
         TestCase {
             name: "array_preserve_original",
             defaults_type: "array",
-            orig_value: "a foo b bar c",
-            orig_check_value: "- a\n- foo\n- b\n- bar\n- c",
-            new_value: "['foo', '...', 'bar', 'baz']",
-            check_value: "(\n    foo,\n    a,\n    b,\n    bar,\n    c,\n    baz\n)",
+            orig_defaults_set_value: "(a, foo, b, bar, c)",
+            orig_up_check_value: "- a\n- foo\n- b\n- bar\n- c",
+            up_set_value: "['foo', '...', 'bar', 'baz']",
+            defaults_check_value: "(\n    foo,\n    a,\n    b,\n    bar,\n    c,\n    baz\n)",
         },
         // Check preserving original dict values works.
         TestCase {
             name: "dict_preserve_original",
             defaults_type: "dict",
-            orig_value: "a 1 foo 2 b 3 bar 4 c 5",
-            orig_check_value: "foo: '2'\nb: '3'\nbar: '4'\nc: '5'\na: '1'",
-            new_value: "{'foo': 6, '...':'...', 'bar': 7, 'baz': 8}",
-            check_value: "{\n    a = 1;\n    b = 3;\n    bar = 4;\n    baz = 8;\n    c = 5;\n    \
-                          foo = 6;\n}",
+            orig_defaults_set_value: "{ a = 1; b = 3; bar = 4; c = 5; foo = 2; }",
+            orig_up_check_value: "foo: '2'\nb: '3'\nbar: '4'\nc: '5'\na: '1'",
+            up_set_value: "{'foo': 6, '...':'...', 'bar': 7, 'baz': 8}",
+            defaults_check_value: "{\n    a = 1;\n    b = 3;\n    bar = 4;\n    baz = 8;\n    c = \
+                                   5;\n    foo = 6;\n}",
+        },
+        // Check preserving dicts in an array of dicts works. See `defaults read -g
+        // NSUserDictionaryReplacementItems`.
+        TestCase {
+            name: "array_of_dict_preserve_original",
+            defaults_type: "array",
+            orig_defaults_set_value: r#"
+(
+    {
+        on = 1;
+        replace = omw;
+        with = "On my way!";
+    },
+    {
+        on = 1;
+        replace = zss;
+        with = "Use \\U2318-\\U21e7-Enter.";
+    },
+    {
+        on = 1;
+        replace = za;
+        with = "https://apple.com/";
+    }
+)
+"#,
+            orig_up_check_value: r#"- replace: omw
+  with: On my way!
+  on: '1'
+- replace: zss
+  with: Use \U2318-\U21e7-Enter.
+  on: '1'
+- replace: za
+  with: https://apple.com/
+  on: '1'"#,
+            up_set_value: r#"
+- replace: omw
+  with: Newer on my way
+  on: '1'
+- ...
+- replace: atm
+  with: at the moment
+  on: 1
+- replace: zss
+  with: Use \U2318-\U21e7-Enter.
+  on: '1'
+"#,
+
+            defaults_check_value: r#"(
+        {
+        on = 1;
+        replace = omw;
+        with = "Newer on my way";
+    },
+        {
+        on = 1;
+        replace = omw;
+        with = "On my way!";
+    },
+        {
+        on = 1;
+        replace = zss;
+        with = "Use \\\\U2318-\\\\U21e7-Enter.";
+    },
+        {
+        on = 1;
+        replace = za;
+        with = "https://apple.com/";
+    },
+        {
+        on = 1;
+        replace = atm;
+        with = "at the moment";
+    }
+)"#,
         },
     ];
 
@@ -213,25 +291,29 @@ fn test_defaults_write_local() {
         let TestCase {
             name,
             defaults_type,
-            orig_value,
-            orig_check_value,
-            new_value,
-            check_value,
+            orig_defaults_set_value,
+            orig_up_check_value,
+            up_set_value,
+            defaults_check_value,
         } = test_case;
 
         info!("Testing default {name}: {test_case:#?}");
 
         {
             debug!("Writing original value for {name}");
-            let values = match defaults_type {
-                "array" | "dict" => orig_value.split_whitespace().collect(),
-                _ => vec![orig_value],
-            };
 
             let defaults_key = format!("defaults_write_local_{name}");
-            let defaults_type = format!("-{defaults_type}");
-            let mut args = vec!["write", &domain, &defaults_key, &defaults_type];
-            args.extend(values);
+            let mut args = vec!["write", &domain, &defaults_key];
+
+            let defaults_type_arg;
+            match defaults_type {
+                "array" | "dict" => (),
+                _ => {
+                    defaults_type_arg = format!("-{defaults_type}");
+                    args.push(&defaults_type_arg)
+                }
+            };
+            args.push(orig_defaults_set_value);
 
             // Write the original value to a test plist file.
             cmd("defaults", &args).run().unwrap();
@@ -248,7 +330,7 @@ fn test_defaults_write_local() {
             ]);
             cmd.assert()
                 .success()
-                .stdout(predicate::str::diff(format!("{orig_check_value}\n")));
+                .stdout(predicate::str::diff(format!("{orig_up_check_value}\n")));
         }
 
         {
@@ -256,7 +338,7 @@ fn test_defaults_write_local() {
             let mut cmd = testutils::test_binary_cmd("up", &temp_dir);
 
             let defaults_key = format!("defaults_write_local_{name}");
-            cmd.args(["defaults", "write", &domain, &defaults_key, new_value]);
+            cmd.args(["defaults", "write", &domain, &defaults_key, up_set_value]);
             cmd.assert()
                 .success()
                 .stderr(predicate::str::contains(format!(
@@ -274,7 +356,7 @@ fn test_defaults_write_local() {
             )
             .read()
             .unwrap();
-            assert_eq!(*check_value, new_default);
+            assert_eq!(*defaults_check_value, new_default);
         }
     }
 }
